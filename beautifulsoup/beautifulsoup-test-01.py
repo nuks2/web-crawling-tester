6d47586b9ebea01a0f6e6fc9eef34b93b6c2be00
nuks2 <inuks2@gmail.com>
2021-09-29 00:22:03 +0900

    예제추가

---
+++ beautifulsoup/beautifulsoup-test-01.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+bsObj = BeautifulSoup(html, "html.parser")
+print(bsObj.h1)
+
+# Exception handling
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        print(e)
+        return None
+    try:
+        bsObj = BeautifulSoup(html, "html.parser")
+        # title = bsObj.body.h1
+        title = bsObj.noTag.someTag
+    except AttributeError as e:
+        print('AttributeError : ', e)
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+# title = getTitle("http://www.pythonscraping.com/exercises/exercise001.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
--- test-05-05.py
+++ test-05-05.py
@@ -13,18 +13,3 @@ for li in lis:
 
 # first_a = top_right.find("a")
 # print(first_a.text)
-
-# <ul class="list_nav type_fix">
-# <li class="nav_item">
-# <a href="https://mail.naver.com/" class="nav" data-clk="svc.mail"><i class="ico_mail"></i>메일</a>
-# </li>
-# <li class="nav_item"><a href="https://section.cafe.naver.com/" class="nav" data-clk="svc.cafe">카페</a></li>
-# <li class="nav_item"><a href="https://section.blog.naver.com/" class="nav" data-clk="svc.blog">블로그</a></li>
-# <li class="nav_item"><a href="https://kin.naver.com/" class="nav" data-clk="svc.kin">지식iN</a></li>
-# <li class="nav_item"><a href="https://shopping.naver.com/" class="nav shop" data-clk="svc.shopping"><span class="blind">쇼핑</span></a></li>
-# <li class="nav_item"><a href="https://shoppinglive.naver.com/home" class="nav shoplive" data-clk="svc.shoppinglive"><span class="blind">쇼핑LIVE</span></a></li>
-# <li class="nav_item"><a href="https://order.pay.naver.com/home" class="nav" data-clk="svc.pay">Pay</a></li>
-# <li class="nav_item">
-# <a href="https://tv.naver.com/" class="nav" data-clk="svc.tvcast"><i class="ico_tv"></i>TV</a>
-# </li>
-# </ul>
