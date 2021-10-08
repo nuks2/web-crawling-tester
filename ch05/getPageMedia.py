9b0aa9d0d4948081b7da5182670c45f90c4efae4
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-10-08 18:45:55 +0900

    추가

--- ch05/getPageMedia.py
+++ ch05/getPageMedia.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com")
 bsObj = BeautifulSoup(html, "html.parser")
-# downloadList = bsObj.findAll(src=True)
-downloadList = bsObj.findAll({src:True})
+downloadList = bsObj.findAll(src=True)
+
 
 for download in downloadList:
     print(download["src"])
