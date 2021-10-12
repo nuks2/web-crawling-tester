d3d57d69dfe3eae0bf4d3ca0bb17fe9c1a886a0c
nuks2 <inuks2@gmail.com>
2021-10-13 00:56:38 +0900

    추가

---
+++ scrapping_with_python/ch05/ch05-01-01.py
@@ -0,0 +1,12 @@
+import os
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+html = urlopen("http://www.pythonscraping.com")
+bsObj = BeautifulSoup(html, "html.parser")
+downloadList = bsObj.findAll(src=True)
+
+for download in downloadList:
+    print(download["src"])
