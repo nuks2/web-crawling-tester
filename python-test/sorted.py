5fbcc29e4dec1a86317efc124c888de4ed4d3ede
Yi Jinuk <jinuk.yi@twolinecode.com>
2021-10-15 16:07:01 +0900

    8장 추가

--- python-test/sorted.py
+++ python-test/sorted.py
@@ -1,11 +1,17 @@
+from operator import itemgetter
+
+
 student_tuples = [
     ('john', 'A', 15),
     ('jane', 'B', 12),
-    ('dave', 'B', 10),]
+    ('dave', 'B', 10),
+]
 
-sorted = sorted(student_tuples, key=lambda student: student[2], reverse=True)
-print(sorted)
-# dic = dict(sorted)
+sorted_list = sorted(student_tuples, key=lambda student: student[2], reverse=True)
+print(sorted_list)
+# dic = dict(sorted_list)
+
+print(sorted(student_tuples, key=itemgetter(0)))
 
 student_tuples_2 = [
     ('john', 'A'),
@@ -14,3 +20,5 @@ student_tuples_2 = [
 
 dic = dict(student_tuples_2)
 print(dic)
+
+
